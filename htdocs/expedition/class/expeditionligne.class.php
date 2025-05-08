59ebf37c64a7cb6dae430db750d3e9a832a77e8b
sonikf <93765174+sonikf@users.noreply.github.com>
2025-05-08 11:07:10 +0300

    Update expeditionligne.class.php

--- htdocs/expedition/class/expeditionligne.class.php
+++ htdocs/expedition/class/expeditionligne.class.php
@@ -374,7 +374,7 @@ class ExpeditionLigne extends CommonObjectLine
 	{
 		global $langs;
 		$error = 0;
-		$origin_id = null;
+		$origin_id = $this->origin_id;
 		if ($origin_id > 0) {
 			// Check parameters
 			if (empty($this->fk_expedition)
@@ -656,7 +656,7 @@ class ExpeditionLigne extends CommonObjectLine
 		$batch = null;
 		$batch_id = 0;
 		$expedition_batch_id = 0;
-		$origin_id = null;
+		$origin_id = $this->origin_id;
 		if ($origin_id > 0) {
 			if (is_array($this->detail_batch)) { 	// array of ExpeditionLineBatch
 				if (count($this->detail_batch) > 1) {
