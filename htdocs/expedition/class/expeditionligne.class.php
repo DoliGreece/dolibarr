fcd27e2d32ff5e1561509fda5728fcd5c5cc4564
sonikf <93765174+sonikf@users.noreply.github.com>
2025-05-08 11:17:47 +0300

    Update expeditionligne.class.php

--- htdocs/expedition/class/expeditionligne.class.php
+++ htdocs/expedition/class/expeditionligne.class.php
@@ -14,7 +14,7 @@
  * Copyright (C) 2018-2025  Frédéric France         <frederic.france@free.fr>
  * Copyright (C) 2020       Lenin Rivas         	<lenin@leninrivas.com>
  * Copyright (C) 2024-2025	MDW						<mdeweerd@users.noreply.github.com>
- * Copyright (C) 2025		Nick Fragoulis			
+ * Copyright (C) 2025		Nick Fragoulis
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
