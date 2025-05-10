c360cb6957ba27da6d7bd6763df3fe734fd18840
sonikf <93765174+sonikf@users.noreply.github.com>
2025-05-10 10:25:11 +0300

    Update expeditionligne.class.php

--- htdocs/expedition/class/expeditionligne.class.php
+++ htdocs/expedition/class/expeditionligne.class.php
@@ -644,7 +644,7 @@ class ExpeditionLigne extends CommonObjectLine
 	 *	@param		int		$notrigger		1 = disable triggers
 	 *  @return		int					Return integer < 0 if KO, > 0 if OK
 	 */
-	public function update(User $user, $notrigger = 0)
+	public function update($user = null, $notrigger = 0)
 	{
 		global $langs;
 		$error = 0;
